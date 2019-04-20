d7566cf8b161fead26b37c3bb360b38c7e6b7dd5
dazzilove <dazzilove@gmail.com>
2019-04-20 15:54:54 +0900

    원격 Product 서비스에 연동하여 관련 정보를 가져오도록 수정

--- order/src/main/java/com/dazzilove/hystrix/order/controller/OrderController.java
+++ order/src/main/java/com/dazzilove/hystrix/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.dazzilove.hystrix.order.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OrderController {
@@ -11,6 +12,9 @@ public class OrderController {
 
 	@RequestMapping(value = "/order/product/{id}", method = RequestMethod.GET)
 	public String getProductById(@PathVariable("id") String id) {
-		return "Order Call - Product Id = " + id;
+
+		RestTemplate restTemplate = new RestTemplate();
+		String product = (String) restTemplate.getForObject("http://localhost:10002/product/" + id, String.class);
+		return "Order Call - Product Id = " + product;
 	}
 }
