54306b739815a24aa6a50db9caeafbdb720994e0
dazzilove <dazzilove@gmail.com>
2019-04-21 03:58:10 +0900

    hystrix + fallback 처리

--- order/src/main/java/com/dazzilove/hystrix/order/controller/OrderController.java
+++ order/src/main/java/com/dazzilove/hystrix/order/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.dazzilove.hystrix.order.controller;
 
+import com.dazzilove.hystrix.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OrderController {
+	@Autowired
+	OrderService orderService;
+
 	@RequestMapping(value = "/order/print", method = RequestMethod.GET)
 	public String print() {
 		return "OrderController Call!";
@@ -11,6 +16,6 @@ public class OrderController {
 
 	@RequestMapping(value = "/order/product/{id}", method = RequestMethod.GET)
 	public String getProductById(@PathVariable("id") String id) {
-		return "Order Call - Product Id = " + id;
+		return "Order Call - Product = " + orderService.getProductById(id);
 	}
 }
