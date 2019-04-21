9ea17d0f19253b622b5d5e527b7f51fe1eb5537f
dazzilove <dazzilove@gmail.com>
2019-04-21 15:56:01 +0900

    hystrix stream을 확인하기위해 springboot actuator를 추가

--- order/src/main/java/com/dazzilove/hystrix/order/OrderApplication.java
+++ order/src/main/java/com/dazzilove/hystrix/order/OrderApplication.java
@@ -3,6 +3,7 @@ package com.dazzilove.hystrix.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
